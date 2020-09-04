7930ad0f0e21238a4feecb9dabc28bc4752a9190
Jacob Schaupp <jacob.schaupp@rockets.utoledo.edu>
2020-09-04 16:17:23 -0400

    Basic Class Structure

---
+++ Classes/Socket.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jacob on 9/4/20.
+//
+
+#include "Socket.h"
+
+//create and bind to the socket
+bool Socket::Initilize() {
+    return true;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <sys/socket.h>
+#include "Classes/Socket.h"
 
 int main() {
-
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
